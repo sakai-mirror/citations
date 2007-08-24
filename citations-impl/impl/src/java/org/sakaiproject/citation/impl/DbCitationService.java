b40152c45437eca22163517a88ea3bd6529a7130
jimeng@umich.edu <jimeng@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-08-24 18:25:16 +0000

    SAK-11153 Changing type of values to String instead of Object when reading from and writing to database. svn merge -r34323:34324 https://source.sakaiproject.org/svn/citations/trunk/
    git-svn-id: https://source.sakaiproject.org/svn/citations/branches/sakai_2-4-x@34387 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- citations-impl/impl/src/java/org/sakaiproject/citation/impl/DbCitationService.java
+++ citations-impl/impl/src/java/org/sakaiproject/citation/impl/DbCitationService.java
@@ -1547,7 +1547,7 @@ public class DbCitationService extends BaseCitationService
          * @param name
          * @param value
          */
-        public Triple(String id, String name, Object value)
+        public Triple(String id, String name, String value)
         {
 	        super();
 	        m_id = id;
@@ -1626,7 +1626,7 @@ public class DbCitationService extends BaseCitationService
             {
 	            String citationId = result.getString(1);
 	            String name = result.getString(2);
-	            Object value = result.getObject(3);
+	            String value = result.getString(3);
 
 	            triple = new Triple(citationId, name, value);
             }
