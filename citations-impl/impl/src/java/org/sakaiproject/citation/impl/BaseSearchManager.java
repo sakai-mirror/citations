6dc29eff242e07e581610d98b0c8f5a9978ab755
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-10-22 22:28:33 +0000

    svn merge -c 37090 https://source.sakaiproject.org/svn/citations/trunk U    citations-api/api/src/java/org/sakaiproject/citation/api/SearchManager.java U    citations-api/api/src/java/org/sakaiproject/citation/api/ActiveSearch.java A    citations-api/api/src/java/org/sakaiproject/citation/util/api/SearchCancelException.java U    citations-api/api/src/java/org/sakaiproject/citation/cover/SearchManager.java U    citations-tool/tool/src/java/org/sakaiproject/citation/tool/CitationHelperAction.java U    citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseSearchManager.java svn log -r 37090 https://source.sakaiproject.org/svn/citations/trunk ------------------------------------------------------------------------ r37090 | gbhatnag@umich.edu | 2007-10-17 12:26:56 -0400 (Wed, 17 Oct 2007) | 1 line
    SAK-11358: accomodating for changes in cancellation code. Instead of throwing SearchException upon cancel, throwing a new Exception, SearchCancelException.
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.sakaiproject.org/svn/citations/branches/sakai_2-5-x@37191 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseSearchManager.java
+++ citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseSearchManager.java
@@ -62,6 +62,7 @@ import org.sakaiproject.authz.api.SecurityAdvisor.SecurityAdvice;
 import org.sakaiproject.authz.cover.SecurityService;
 import org.sakaiproject.citation.util.api.CQLSearchQuery;
 import org.sakaiproject.citation.util.api.OsidConfigurationException;
+import org.sakaiproject.citation.util.api.SearchCancelException;
 import org.sakaiproject.citation.util.api.SearchQuery;
 import org.sakaiproject.citation.api.ActiveSearch;
 import org.sakaiproject.citation.api.Citation;
@@ -289,7 +290,7 @@ public class BaseSearchManager implements SearchManager, Observer
     	 * @return
     	 * @throws SearchException
     	 */
-    	public List viewPage(int page) throws SearchException
+    	public List viewPage(int page) throws SearchException, SearchCancelException
     	{
     		List citations = new Vector();
 
@@ -770,7 +771,7 @@ public class BaseSearchManager implements SearchManager, Observer
 		/* (non-Javadoc)
          * @see org.sakaiproject.citation.api.ActiveSearch#viewPage()
          */
-        public List viewPage() throws SearchException
+        public List viewPage() throws SearchException, SearchCancelException
         {
         	return viewPage(0);
         }
@@ -1953,7 +1954,7 @@ public class BaseSearchManager implements SearchManager, Observer
      * @see org.sakaiproject.citation.impl.SearchManager#doSearch(org.sakaiproject.citation.api.ActiveSearch)
      */
 	public ActiveSearch doSearch(ActiveSearch search)
-		throws SearchException
+		throws SearchException, SearchCancelException
 	{
 		// search parameters
 		Integer    pageSize       = search.getPageSize();
@@ -2104,7 +2105,7 @@ public class BaseSearchManager implements SearchManager, Observer
 	    					catch( InterruptedException ie )
 	    					{
 	    						// search canceled
-	    						throw new SearchException( "search canceled" );
+	    						throw new SearchCancelException();
 	    					}
 	    				}
 	    			}
